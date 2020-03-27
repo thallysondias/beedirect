bdea8abecfd46e6e7c127f4f3ba86d9037197b91
Thallyson Dias <48736970+thallysondiasdmc@users.noreply.github.com>
2020-03-27 17:15:48 +0000

    hide loading image

--- widgets/price/index.php
+++ widgets/price/index.php
@@ -229,7 +229,7 @@ class beePrice extends Widget_Base {
               $('.omnibees-best-price').html(bestPriceApi);
             }else {
               console.log("O hotel não possui tarifa para hoje");
-              $(".beedirect__best-price").hide();
+              $(".omnibees-best-price img").hide();
               $(".static-price").show();
             }
         });
