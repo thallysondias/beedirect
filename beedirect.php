5c956006362d5ddebd4eb084c8223f2566718060
Thallyson Dias <48736970+thallysondiasdmc@users.noreply.github.com>
2020-03-27 12:51:25 +0000

    Update beedirect.php

--- beedirect.php
+++ beedirect.php
@@ -4,7 +4,7 @@ ob_start();
   Plugin name: BeeDirect by Omnibees
   Plugin uri: widgets.omnibees.com/manual
   Description: Widgets to BeeDirect
-  Version: 0.0.2
+  Version: 0.0.3
   Author: Omnibees
   Author uri: www.omnibees.com
   License: GPlv2 or Later
