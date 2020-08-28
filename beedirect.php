cd980b6fe9895f55df4a18c740416f99bfbf1f9d
Artem Tkachuk <artemtkachuk1996@gmail.com>
2020-08-28 10:34:00 +0100

    Layout quartos - novo

--- beedirect.php
+++ beedirect.php
@@ -3,7 +3,7 @@
   Plugin name: BeeDirect by Omnibees
   Plugin uri: widgets.omnibees.com/manual
   Description: Widgets to BeeDirect
-  Version: 1.4.0
+  Version: 1.4.1
   Author: Omnibees
   Author uri: www.omnibees.com
   License: GPlv2 or Later
